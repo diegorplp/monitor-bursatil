0e14173d888c4333995d00e3e4c01e7faa94d413
diegorplp <diego_rp_94@live.com>
2025-12-10 15:44:12 -0300

    Add files via upload

--- data_client.py
+++ data_client.py
@@ -8,6 +8,7 @@ import os
 import json 
 import streamlit as st 
 import time # Para el sleep
+import io # CRÍTICO: Necesario para el String.IO
 
 pd.options.mode.chained_assignment = None 
 IOL_BASE_URL = "https://api.invertironline.com"
@@ -121,7 +122,6 @@ def get_history_yahoo(tickers_list):
     return df_final.fillna(method='ffill')
 
 # --- ORQUESTADOR PRINCIPAL (CACHEADO) ---
-# Quitamos el @st.cache_data de la función principal
 def get_data(lista_tickers=None):
     tickers_target = lista_tickers if lista_tickers else TICKERS
     if not tickers_target: return pd.DataFrame()
@@ -137,13 +137,14 @@ def get_data(lista_tickers=None):
                 
                 # Si el archivo es de hoy, cargamos histórico (Cache de Supervivencia)
                 if cache_date == today.date():
-                    df_history = pd.read_json(cache_data['data'])
+                    # CRÍTICO: Usar StringIO para eliminar el FutureWarning
+                    df_history = pd.read_json(io.StringIO(cache_data['data'])) 
                     print("   [Cache] Histórico cargado de archivo JSON de hoy.")
                     
                     # 2. Obtener Precios de Hoy (Siempre Real Time)
                     dict_precios_hoy = get_current_prices_iol(tickers_target)
                     
-                    if not dict_precios_hoy: return pd.DataFrame() # No hay precio hoy, no podemos calcular
+                    if not dict_precios_hoy: return pd.DataFrame() 
                     
                     # Fusionar con la fila de hoy
                     row_today = pd.DataFrame([dict_precios_hoy], index=[today])
@@ -184,7 +185,6 @@ def get_data(lista_tickers=None):
     cutoff = datetime.now() - timedelta(days=DIAS_HISTORIAL)
     df_final = df_final[df_final.index >= cutoff]
 
-    # 4. ESCRIBIR EL CACHÉ JSON (Cache de Supervivencia)
     try:
         if not os.path.exists(CACHE_DIR): os.makedirs(CACHE_DIR)
         
